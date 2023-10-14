e1677c5a98f4f46f4c30679ca090d3810966e56f
tcweeei <open891013@gmail.com>
2023-10-14 23:07:45 +0800

    Update the route.

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,23 @@ import Build_transaction from "./pages/build_transaction";
 import Interact_contract from "./pages/interact_contract";
 const router=createBrowserRouter([
     {
-        path:'/',
+        path:'/dapp_website_react/',
         element:<Home></Home>
     },
     {
-        path:'/connect_wallet',
+        path:'/dapp_website_react/connect_wallet',
         element:<Connect_wallet></Connect_wallet>
     },
     {
-        path:"/get_information",
+        path:"/dapp_website_react/get_information",
         element:<Get_information></Get_information>
     },
     {
-        path:"build_transaction",
+        path:"/dapp_website_react/build_transaction",
         element:<Build_transaction></Build_transaction>
     },
     {
-        path:"interact_contract",
+        path:"/dapp_website_react/interact_contract",
         element:<Interact_contract></Interact_contract>
     }
 ])
