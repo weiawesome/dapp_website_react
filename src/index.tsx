60034fe3aa1b4d08991884d61d45c7e911d2c319
tcweeei <open891013@gmail.com>
2023-10-16 23:30:18 +0800

    Add the index import test.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './react-app-env'
+import './react-app-env.d'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
